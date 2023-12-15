800b87b935145a050ff9f09cf87b86b5bccd65a3
sandunrmst <sandunalugc@gmail.com>
2023-12-15 13:58:48 +0530

    fix ui issues with testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Nav from "./components/Nav"
 import Header from "./components/Header"
 import Menu from './components/Menu';
+import Testimonials from './components/Testimonials';
 
 
 
@@ -13,6 +14,7 @@ function App() {
       <Nav/>
       <Header/>
       <Menu/>
+      <Testimonials/>
       
 
     </>
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,3 +1,6 @@
+import img1 from "../images/reviewer1.png"
+import img2 from "../images/reviewer2.png"
+import img3 from "../images/reviewer3.png"
 
 
 export default function Testimonials() {
@@ -6,39 +9,36 @@ export default function Testimonials() {
         <p className="testimonials-title">Testimonials</p>
         <div className="testimonials">
             <div className="testimonial">
-                <img className="reviewer-img" src="../images/reviewer1.png" alt="greek salad"/>
+                <img className="reviewer-img" src={img1} alt="greek salad"/>
                 <div className="name-rating">
                     <p className="reviewer-name">John Doe</p>
                     <p className="reviewer-rating">5/5</p>
                 </div>
                 <div className="review-text">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa massa, 
-                        semper sed enim at, faucibus mattis lorem. Donec vehicula, leo ut auctor ullamcorper, 
-                        diam tortor dignissim velit, ac iaculis libero lacus non diam. </p>
+                        semper sed, faucibus mattis lorem. </p>
                 </div>
             </div>
             <div className="testimonial">
-                <img className="reviewer-img" src="../images/reviewer2.png" alt="greek salad"/>
+                <img className="reviewer-img" src={img2} alt="greek salad"/>
                 <div className="name-rating">
                     <p className="reviewer-name">John Doe</p>
                     <p className="reviewer-rating">5/5</p>
                 </div>
                 <div className="review-text">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa massa, 
-                        semper sed enim at, faucibus mattis lorem. Donec vehicula, leo ut auctor ullamcorper, 
-                        diam tortor dignissim velit, ac iaculis libero lacus non diam. </p>
+                        semper sed enim at, iaculis libero lacus non diam. </p>
                 </div>
             </div>
             <div className="testimonial">
-                <img className="reviewer-img" src="../images/reviewer3.png" alt="greek salad"/>
+                <img className="reviewer-img" src={img3} alt="greek salad"/>
                 <div className="name-rating">
                     <p className="reviewer-name">John Doe</p>
                     <p className="reviewer-rating">5/5</p>
                 </div>
                 <div className="review-text">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa massa, 
-                        semper sed enim at, faucibus mattis lorem. Donec vehicula, leo ut auctor ullamcorper, 
-                        diam tortor dignissim velit, ac iaculis libero lacus non diam. </p>
+                    iaculis libero lacus non diam. </p>
                 </div>
             </div>
         </div>
